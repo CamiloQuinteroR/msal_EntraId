2dcd357be108ab8d1e0413407f527c1b9c6f6d91
camiloaqrz14@gmail.com <camiloaqrz14@gmail.com>
2025-10-14 21:42:44 -0500

    modificación de id's

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,8 @@
 export const msalConfig = {
     auth: {
-      clientId: "b202ea33-7e7d-4248-a9d0-5db1290d7aa0", // <-- tu Application (client) ID
-      authority: "https://login.microsoftonline.com/cb4c3445-d1c4-49be-8835-70ae5d72c777", // <-- tu Tenant ID
-      redirectUri: "http://localhost:5173/", // debe coincidir con tu URI registrada
+      clientId: "0b57c4bf-a8e8-4296-83f5-7d268aca5111", //  (client) ID
+      authority: "https://login.microsoftonline.com/cb4c3445-d1c4-49be-8835-70ae5d72c777", //  Tenant ID
+      redirectUri: "http://localhost:5173/", 
     },
   };
   
