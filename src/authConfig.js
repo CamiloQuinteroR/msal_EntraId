e1d4d91c918b353cc8f661ef79db6d3b39007683
camiloaqrz14@gmail.com <camiloaqrz14@gmail.com>
2025-10-17 23:11:18 -0500

    Comentado

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,11 +1,12 @@
 export const msalConfig = {
     auth: {
-      clientId: "0b57c4bf-a8e8-4296-83f5-7d268aca5111", //  (client) ID
-      authority: "https://login.microsoftonline.com/cb4c3445-d1c4-49be-8835-70ae5d72c777", //  Tenant ID
-      redirectUri: "http://localhost:5173/", 
+      clientId: "0b57c4bf-a8e8-4296-83f5-7d268aca5111", //  identificador único de la aplicación
+      authority: "https://login.microsoftonline.com/cb4c3445-d1c4-49be-8835-70ae5d72c777", //  en qué organización debe buscar al usuario para autenticarlo
+      redirectUri: "http://localhost:5173/", //dirección donde volverá el usuario después del login exitoso
     },
   };
   
+  // Solicitud de permisos
   export const loginRequest = {
     scopes: ["User.Read"], // Permiso básico para leer info del usuario
   };
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication } from "@azure/msal-browser"; // biblioteca oficial para manejar autenticación en el navegador
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig.js";
 
-const msalInstance = new PublicClientApplication(msalConfig);
+const msalInstance = new PublicClientApplication(msalConfig); //Creamos la instancia de MSAL
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
